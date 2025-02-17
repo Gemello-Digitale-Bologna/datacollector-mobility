ecacd6c574d1c8166ceea0814e7cc658f5d6b8e1
Nawaz Khurshid <nawaz1981@gmail.com>
2025-02-17 12:36:01 +0100

    Fix for refactoration of parking data as per digital platform project context.

--- src/download-parking.py
+++ src/download-parking.py
@@ -1,4 +1,4 @@
-import mlrun
+
 import pandas as pd
 import geopandas as gpd
 import requests
@@ -8,11 +8,10 @@ import os
 
 base_url = "https://opendata.comune.bologna.it/api/explore/v2.1/catalog/datasets/{name}/exports/geojson?lang=it&timezone=UTC"
 
-s3_bucket = "dataspace"
 s3_path = "mobility-data"
 path_latest = "latest"
 
-def s3_store(s3_client, local_file, fname, year, latest):
+def s3_store(s3_client, s3_bucket, local_file, fname, year, latest):
     s3_client.upload_file(local_file, s3_bucket, s3_path + '/parking-availability/' + str(year) + '/' + fname,
                           ExtraArgs={'ContentType':'application/octet-stream'})
     if latest:
@@ -20,14 +19,14 @@ def s3_store(s3_client, local_file, fname, year, latest):
                               ExtraArgs={'ContentType':'application/octet-stream'})
         
 
-def s3_read(s3_client, local_file, fname, year):
+def s3_read(s3_client, s3_bucket, local_file, fname, year):
     try:
         s3_client.download_file(s3_bucket, s3_path + '/parking-availability/' + str(year) + '/' + fname, local_file)
     except:
         print(f"file not exists on S3 {year} {fname}")
        
     
-def write_year(context, s3_client, actual_df, year, fname, latest):
+def write_year(project, s3_bucket, s3_client, actual_df, year, fname, latest):
     #filter by year
     rslt_df = actual_df[actual_df['anno'] == year]
     
@@ -36,7 +35,7 @@ def write_year(context, s3_client, actual_df, year, fname, latest):
         os.makedirs('./data/'+str(year))
 
     parquet_file = './data/'+str(year)+'/'+fname+'.parquet'
-    s3_read(s3_client, parquet_file, fname+'.parquet', year)
+    s3_read(s3_client, s3_bucket, parquet_file, fname+'.parquet', year)
     if os.path.exists(parquet_file):
         old_df = gpd.read_parquet(parquet_file)
         new_df = pd.concat([old_df, rslt_df], ignore_index=True).drop_duplicates(subset=['guid', 'data'])
@@ -44,16 +43,13 @@ def write_year(context, s3_client, actual_df, year, fname, latest):
     else:
         rslt_df.to_parquet(parquet_file)
 
-    s3_store(s3_client, parquet_file, fname+'.parquet', year, latest)
-    
-    with open(parquet_file, 'rb') as in_file:
-        artifact_name = str(year) + '-' + fname
-        content = in_file.read()
-        context.log_artifact(artifact_name, body=content, format="parquet", db_key=artifact_name)
+    s3_store(s3_client, s3_bucket, parquet_file, fname+'.parquet', year, latest)
+
+    artifact_name = str(year) + '-' + fname
+    project.log_artifact(name=artifact_name, kind="artifact", source=parquet_file)   
     
     
-@mlrun.handler()
-def download_parking_availability(context):
+def download_parking_availability(project, bucket):
     s3 = boto3.client('s3',
                     endpoint_url=os.environ.get('S3_ENDPOINT_URL'),
                     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
@@ -73,4 +69,4 @@ def download_parking_availability(context):
        
     years = gdf['anno'].unique()
     for index, year in enumerate(years):
-        write_year(context, s3, gdf, year, name, (index==(len(years)-1)))
+        write_year(project, bucket, s3, gdf, year, name, (index==(len(years)-1)))
