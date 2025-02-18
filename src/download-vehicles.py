ce675eedd439631d3bb917f87498d7ee971e90cb
Nawaz Khurshid <nawaz1981@gmail.com>
2025-02-18 11:39:32 +0100

    Fix for transformation of vehicles data script in dh platform project

--- src/download-vehicles.py
+++ src/download-vehicles.py
@@ -1,4 +1,4 @@
-import mlrun
+# import mlrun
 import requests
 import boto3
 import os
@@ -7,34 +7,28 @@ from datetime import datetime
 
 base_url = "https://opendata.comune.bologna.it/api/explore/v2.1/catalog/datasets/{name}/exports/parquet?lang=it&refine=anno%3A%22{year}%22&timezone=Europe%2FRome"
 
-s3_bucket = "dataspace"
 s3_path = "mobility-data"
 path_latest = "latest"
 
-def s3_store(s3_client, local_file, fname, year, latest):
+def s3_store(s3_client, s3_bucket, local_file, fname, year, latest):
     s3_client.upload_file(local_file, s3_bucket, s3_path + '/vehicles/' + str(year) + '/' + fname,
                           ExtraArgs={'ContentType':'application/octet-stream'})
     if latest:
         s3_client.upload_file(local_file, s3_bucket, s3_path + '/vehicles/' + path_latest + '/' + fname,
                           ExtraArgs={'ContentType':'application/octet-stream'})
-        
-        
+                
 
         
-def write_year(context, s3_client, year, name, parquet_file, latest):    
-    s3_store(s3_client, parquet_file, name+'.parquet', year, latest)
-    
-    with open(parquet_file, 'rb') as in_file:
-        artifact_name = str(year) + '-' + name
-        content = in_file.read()
-        context.log_artifact(artifact_name, body=content, format="parquet", db_key=artifact_name)
+def write_year(project, s3_bucket, s3_client, year, name, parquet_file, latest):    
+    s3_store(s3_client, s3_bucket, parquet_file, name+'.parquet', year, latest)
+    project.log_artifact(name=name, kind="artifact", source=parquet_file)
     
     print(f"write vehicles data year {year}")
     
     
     
-@mlrun.handler()
-def download_vehicles_by_year(context, year):
+# @mlrun.handler()
+def download_vehicles_by_year(project, bucket, year):
     s3 = boto3.client('s3',
                     endpoint_url=os.environ.get('S3_ENDPOINT_URL'),
                     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
@@ -57,6 +51,5 @@ def download_vehicles_by_year(context, year):
         with open(parquet_file, 'bw') as out_file:
             out_file.write(response.content)
     
-        write_year(context, s3, year, name, parquet_file, False)
-        
-        
+        write_year(project, bucket, s3, year, name, parquet_file, False)
+
