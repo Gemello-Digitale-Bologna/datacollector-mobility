e6e586f5773c9e180cfcec28558c8a0f87cf5395
Nawaz Khurshid <nawaz1981@gmail.com>
2025-02-19 12:47:46 +0100

    Fixed for transformation of bike data

--- src/download-bike.py
+++ src/download-bike.py
@@ -1,4 +1,3 @@
-import mlrun
 import requests
 import boto3
 import os
@@ -7,11 +6,10 @@ from datetime import datetime
 
 base_url = "https://opendata.comune.bologna.it/api/explore/v2.1/catalog/datasets/{name}/exports/parquet?lang=it&refine=data%3A%22{year}%22&timezone=Europe%2FRome"
 
-s3_bucket = "dataspace"
 s3_path = "mobility-data"
 path_latest = "latest"
 
-def s3_store(s3_client, local_file, fname, year, latest):
+def s3_store(s3_client, s3_bucket, local_file, fname, year, latest):
     s3_client.upload_file(local_file, s3_bucket, s3_path + '/bike-flow/' + str(year) + '/' + fname,
                           ExtraArgs={'ContentType':'application/octet-stream'})
     if latest:
@@ -21,20 +19,11 @@ def s3_store(s3_client, local_file, fname, year, latest):
         
 
         
-def write_year(context, s3_client, year, name, parquet_file, latest):    
-    s3_store(s3_client, parquet_file, name+'.parquet', year, latest)
-    
-    with open(parquet_file, 'rb') as in_file:
-        artifact_name = str(year) + '-' + name
-        content = in_file.read()
-        context.log_artifact(artifact_name, body=content, format="parquet", db_key=artifact_name)
-    
+def write_year(bucket, s3_client, year, name, parquet_file, latest):    
+    s3_store(s3_client, bucket, parquet_file, name+'.parquet', year, latest)
     print(f"write bike data year {year}")
     
-    
-    
-@mlrun.handler()
-def download_bike_flow_by_year(context, year):
+def download_bike_flow_by_year(project, bucket, year):
     s3 = boto3.client('s3',
                     endpoint_url=os.environ.get('S3_ENDPOINT_URL'),
                     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
@@ -49,6 +38,7 @@ def download_bike_flow_by_year(context, year):
     name = "colonnine-conta-bici"
     parquet_file = './data/'+str(year)+'/'+name+'.parquet'
 
+    print(base_url.format(name = name, year = year))
     response = requests.get(base_url.format(name = name, year = year))
     
     if response.status_code == 200:
@@ -57,9 +47,7 @@ def download_bike_flow_by_year(context, year):
     
         write_year(context, s3, year, name, parquet_file, False)
         
-        
-@mlrun.handler()
-def download_bike_flow_latest(context):
+def download_bike_flow_latest(project, bucket, run):
     year = datetime.now().year
     s3 = boto3.client('s3',
                     endpoint_url=os.environ.get('S3_ENDPOINT_URL'),
@@ -75,11 +63,16 @@ def download_bike_flow_latest(context):
     name = "colonnine-conta-bici"
     parquet_file = './data/'+str(year)+'/'+name+'.parquet'
 
+    print(base_url.format(name = name, year = year))
     response = requests.get(base_url.format(name = name, year = year))
     
     if response.status_code == 200:
         with open(parquet_file, 'bw') as out_file:
             out_file.write(response.content)
     
-        write_year(context, s3, year, name, parquet_file, True)
+        write_year(bucket, s3, year, name, parquet_file, True)
+        
+
+    project.log_artifact(name=name, kind="artifact", source=parquet_file)
+    run.log_metric('epoch', 1)
     
